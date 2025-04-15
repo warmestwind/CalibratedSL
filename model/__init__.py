8b52b0d9f2e5962a663ad585c38dc10375a8eb07
Yong Feng <286606198@qq.com>
2025-04-15 16:12:02 +0800

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,1 +1,56 @@
+import os
 
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel as P
+
+from .U_CSL_US import Unet
+
+
+class Model(nn.Module):
+    def __init__(self, config):
+        super(Model, self).__init__()
+        print('Making model...')
+
+        self.is_train = config.is_train
+        self.num_gpu = config.num_gpu
+        self.uncertainty = config.uncertainty
+        self.n_samples = config.n_samples
+
+        self.model = Unet(config).to(config.device)
+
+
+
+    def forward(self, input):
+        if self.model.training:
+            if self.num_gpu > 1:
+                return P.data_parallel(self.model, input,
+                                       list(range(self.num_gpu)))
+            else:
+                return self.model.forward(input)
+        else:
+            return self.model.forward
+
+
+
+    def save(self, ckpt, epoch):
+        save_dirs = [os.path.join(ckpt.model_dir, 'model_latest.pt')]
+        save_dirs.append(
+            os.path.join(ckpt.model_dir, 'model_{}.pt'.format(epoch)))
+        for s in save_dirs:
+            torch.save(self.model.state_dict(), s)
+
+    def load(self, ckpt, cpu=False):
+        epoch = ckpt.last_epoch
+        kwargs = {}
+        if cpu:
+            kwargs = {'map_location': lambda storage, loc: storage}
+        if epoch == -1:
+            load_from = torch.load(
+                os.path.join(ckpt.model_dir, 'model_latest.pt'), **kwargs)
+        else:
+            load_from = torch.load(
+                os.path.join(ckpt.model_dir, 'model_{}.pt'.format(epoch)), **kwargs)
+        if load_from:
+            self.model.load_state_dict(load_from, strict=False)
