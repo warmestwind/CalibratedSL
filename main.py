f97c6a9ee208c2b72f5e43f3b21d02e6f41babb1
Yong Feng <286606198@qq.com>
2025-04-15 14:15:52 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import torch
+
+from config import get_config
+from dataset import get_dataloader
+from solver_for_evi_sep import Operator
+from util import Checkpoint
+
+
+def main(config):
+    config.device = torch.device('cuda:{}'.format(config.gpu)
+                                 if torch.cuda.is_available() else 'cpu')
+
+    # load data_loader
+    data_loader = get_dataloader(config)
+    check_point = Checkpoint(config)
+    operator = Operator(config, check_point)
+
+    if config.is_train:
+        operator.train(data_loader)
+    else:
+        operator.test(data_loader)
+
+
+
+
+if __name__ == "__main__":
+    config = get_config()
+    main(config)
